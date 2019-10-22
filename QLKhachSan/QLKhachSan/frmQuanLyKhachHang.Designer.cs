00c0615640b53fb91c302ddf4ab0b0ecf4c54d68
hoangduy2910 <hoangduy29101999@gmail.com>
2019-10-22 12:25:55 +0700

    Giao dien Quan ly khach hang

---
+++ QLKhachSan/QLKhachSan/frmQuanLyKhachHang.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace QLKhachSan
+{
+    partial class frmQuanLyKhachHang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbQLKH = new System.Windows.Forms.Label();
+            this.lbHoTenKH = new System.Windows.Forms.Label();
+            this.lbDiaChiKH = new System.Windows.Forms.Label();
+            this.lbQueQuanKH = new System.Windows.Forms.Label();
+            this.lbNgaySinhKH = new System.Windows.Forms.Label();
+            this.lbSoDTKH = new System.Windows.Forms.Label();
+            this.lbSoCMNDKH = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.maKH = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.hoTenKH = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.diaChiKH = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.queQuanKH = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ngaySinhKH = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.soDTKH = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.soCMNDKH = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lbQLKH
+            // 
+            this.lbQLKH.AutoSize = true;
+            this.lbQLKH.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbQLKH.ForeColor = System.Drawing.Color.Red;
+            this.lbQLKH.Location = new System.Drawing.Point(229, 9);
+            this.lbQLKH.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbQLKH.Name = "lbQLKH";
+            this.lbQLKH.Size = new System.Drawing.Size(237, 25);
+            this.lbQLKH.TabIndex = 0;
+            this.lbQLKH.Text = "QUẢN LÝ KHÁCH HÀNG";
+            // 
+            // lbHoTenKH
+            // 
+            this.lbHoTenKH.AutoSize = true;
+            this.lbHoTenKH.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbHoTenKH.ForeColor = System.Drawing.Color.Blue;
+            this.lbHoTenKH.Location = new System.Drawing.Point(18, 72);
+            this.lbHoTenKH.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbHoTenKH.Name = "lbHoTenKH";
+            this.lbHoTenKH.Size = new System.Drawing.Size(77, 20);
+            this.lbHoTenKH.TabIndex = 0;
+            this.lbHoTenKH.Text = "Họ và tên";
+            // 
+            // lbDiaChiKH
+            // 
+            this.lbDiaChiKH.AutoSize = true;
+            this.lbDiaChiKH.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbDiaChiKH.ForeColor = System.Drawing.Color.Blue;
+            this.lbDiaChiKH.Location = new System.Drawing.Point(18, 126);
+            this.lbDiaChiKH.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbDiaChiKH.Name = "lbDiaChiKH";
+            this.lbDiaChiKH.Size = new System.Drawing.Size(57, 20);
+            this.lbDiaChiKH.TabIndex = 0;
+            this.lbDiaChiKH.Text = "Địa chỉ";
+            // 
+            // lbQueQuanKH
+            // 
+            this.lbQueQuanKH.AutoSize = true;
+            this.lbQueQuanKH.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbQueQuanKH.ForeColor = System.Drawing.Color.Blue;
+            this.lbQueQuanKH.Location = new System.Drawing.Point(18, 181);
+            this.lbQueQuanKH.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbQueQuanKH.Name = "lbQueQuanKH";
+            this.lbQueQuanKH.Size = new System.Drawing.Size(79, 20);
+            this.lbQueQuanKH.TabIndex = 0;
+            this.lbQueQuanKH.Text = "Quê quán";
+            // 
+            // lbNgaySinhKH
+            // 
+            this.lbNgaySinhKH.AutoSize = true;
+            this.lbNgaySinhKH.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbNgaySinhKH.ForeColor = System.Drawing.Color.Blue;
+            this.lbNgaySinhKH.Location = new System.Drawing.Point(388, 72);
+            this.lbNgaySinhKH.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbNgaySinhKH.Name = "lbNgaySinhKH";
+            this.lbNgaySinhKH.Size = new System.Drawing.Size(78, 20);
+            this.lbNgaySinhKH.TabIndex = 0;
+            this.lbNgaySinhKH.Text = "Ngày sinh";
+            // 
+            // lbSoDTKH
+            // 
+            this.lbSoDTKH.AutoSize = true;
+            this.lbSoDTKH.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbSoDTKH.ForeColor = System.Drawing.Color.Blue;
+            this.lbSoDTKH.Location = new System.Drawing.Point(388, 126);
+            this.lbSoDTKH.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbSoDTKH.Name = "lbSoDTKH";
+            this.lbSoDTKH.Size = new System.Drawing.Size(102, 20);
+            this.lbSoDTKH.TabIndex = 0;
+            this.lbSoDTKH.Text = "Số điện thoại";
+            // 
+            // lbSoCMNDKH
+            // 
+            this.lbSoCMNDKH.AutoSize = true;
+            this.lbSoCMNDKH.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbSoCMNDKH.ForeColor = System.Drawing.Color.Blue;
+            this.lbSoCMNDKH.Location = new System.Drawing.Point(388, 181);
+            this.lbSoCMNDKH.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbSoCMNDKH.Name = "lbSoCMNDKH";
+            this.lbSoCMNDKH.Size = new System.Drawing.Size(80, 20);
+            this.lbSoCMNDKH.TabIndex = 0;
+            this.lbSoCMNDKH.Text = "Số CMND";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(212, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(115, 123);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(212, 26);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(115, 178);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(212, 26);
+            this.textBox3.TabIndex = 1;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(497, 178);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(212, 26);
+            this.textBox4.TabIndex = 1;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(497, 123);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(212, 26);
+            this.textBox5.TabIndex = 1;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(497, 69);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(212, 26);
+            this.textBox6.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.maKH,
+            this.hoTenKH,
+            this.diaChiKH,
+            this.queQuanKH,
+            this.ngaySinhKH,
+            this.soDTKH,
+            this.soCMNDKH});
+            this.listView1.Location = new System.Drawing.Point(22, 264);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(686, 228);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // maKH
+            // 
+            this.maKH.Text = "Mã KH";
+            this.maKH.Width = 69;
+            // 
+            // hoTenKH
+            // 
+            this.hoTenKH.Text = "Họ và tên";
+            this.hoTenKH.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.hoTenKH.Width = 120;
+            // 
+            // diaChiKH
+            // 
+            this.diaChiKH.Text = "Địa chỉ";
+            this.diaChiKH.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.diaChiKH.Width = 98;
+            // 
+            // queQuanKH
+            // 
+            this.queQuanKH.Text = "Quê quán";
+            this.queQuanKH.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.queQuanKH.Width = 107;
+            // 
+            // ngaySinhKH
+            // 
+            this.ngaySinhKH.Text = "Ngày sinh";
+            this.ngaySinhKH.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.ngaySinhKH.Width = 99;
+            // 
+            // soDTKH
+            // 
+            this.soDTKH.Text = "Số điện thoại";
+            this.soDTKH.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.soDTKH.Width = 118;
+            // 
+            // soCMNDKH
+            // 
+            this.soCMNDKH.Text = "Số CMND";
+            this.soCMNDKH.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.soCMNDKH.Width = 116;
+            // 
+            // frmQuanLyKhachHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 507);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbSoCMNDKH);
+            this.Controls.Add(this.lbQueQuanKH);
+            this.Controls.Add(this.lbSoDTKH);
+            this.Controls.Add(this.lbDiaChiKH);
+            this.Controls.Add(this.lbNgaySinhKH);
+            this.Controls.Add(this.lbHoTenKH);
+            this.Controls.Add(this.lbQLKH);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.ForeColor = System.Drawing.Color.Blue;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmQuanLyKhachHang";
+            this.Text = "frmQuanLyKhachHang";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbQLKH;
+        private System.Windows.Forms.Label lbHoTenKH;
+        private System.Windows.Forms.Label lbDiaChiKH;
+        private System.Windows.Forms.Label lbQueQuanKH;
+        private System.Windows.Forms.Label lbNgaySinhKH;
+        private System.Windows.Forms.Label lbSoDTKH;
+        private System.Windows.Forms.Label lbSoCMNDKH;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader maKH;
+        private System.Windows.Forms.ColumnHeader hoTenKH;
+        private System.Windows.Forms.ColumnHeader diaChiKH;
+        private System.Windows.Forms.ColumnHeader queQuanKH;
+        private System.Windows.Forms.ColumnHeader ngaySinhKH;
+        private System.Windows.Forms.ColumnHeader soDTKH;
+        private System.Windows.Forms.ColumnHeader soCMNDKH;
+    }
+}
